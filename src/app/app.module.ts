9c4671fa483c398efcfb5f395fd0a02fff78b369
Alshymaa <shamscs85@gmail.com>
2018-10-14 22:32:15 +1000

    planing the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { TrainingComponent } from './training/training.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
